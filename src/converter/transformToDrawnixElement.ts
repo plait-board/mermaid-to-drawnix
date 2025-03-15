cf494eb8a36dd0ffa535384b0afc160e7efd4626
pubuzhixing8 <pubuzhixing@gmail.com>
2025-03-15 21:53:36 +0800

    feat: handle group for sequence

--- src/converter/transformToDrawnixElement.ts
+++ src/converter/transformToDrawnixElement.ts
@@ -164,49 +164,6 @@ export const transformToDrawnixContainerElement = (
   return container;
 };
 
-export const transformToDrawnixEllipseElement = (
-  element: Exclude<Node, Line | Arrow | Text>,
-  config: DrawnixConfig
-) => {
-  const styleOptions: Partial<PlaitCommonGeometry> = {
-    strokeStyle: (element?.strokeStyle || StrokeStyle.solid) as StrokeStyle,
-    strokeWidth: element?.strokeWidth || 1,
-    strokeColor:
-      element?.strokeColor ||
-      "hsl(259.6261682243, 59.7765363128%, 87.9019607843%)",
-    fill: element?.bgColor || "#ECECFF",
-  };
-  const textStyle: Partial<CustomText> = {
-    color: element.label?.color || "#000",
-  };
-  const verticesText = buildText(
-    normalizeText(element?.label?.text || ""),
-    undefined,
-    textStyle
-  );
-  const textSize = measureElement(verticesText, {
-    fontFamily: DEFAULT_FONT_FAMILY,
-    fontSize: config.fontSize,
-  });
-  const rectangle = getRectangleByMermaidElement({
-    ...element,
-    width: element.width || element.height,
-  });
-  const container = createGeometryElement(
-    element.type as BasicShapes,
-    RectangleClient.getPoints(rectangle),
-    verticesText,
-    {
-      ...styleOptions,
-    },
-    {
-      textHeight: textSize.height,
-    }
-  );
-
-  return container;
-};
-
 export const transformToDrawnixTextElement = (
   element: Text,
   config: DrawnixConfig
--- src/converter/types/sequence.ts
+++ src/converter/types/sequence.ts
@@ -1,14 +1,22 @@
 import { GraphConverter } from "../GraphConverter.js";
 import { Sequence } from "../../parser/sequence.js";
-import { PlaitElement } from "@plait/core";
+import { PlaitElement, RectangleClient } from "@plait/core";
 import { DrawnixConfig } from "../../index.js";
 import {
   transformToDrawnixArrowElement,
   transformToDrawnixContainerElement,
-  transformToDrawnixEllipseElement,
   transformToDrawnixLineElement,
   transformToDrawnixTextElement,
 } from "../transformToDrawnixElement.js";
+import {
+  BasicShapes,
+  createGeometryElement,
+  getTextShapeProperty,
+  PlaitCommonGeometry,
+  PlaitGeometry,
+} from "@plait/draw";
+import { Element, Node } from "slate";
+import { buildText } from "@plait/common";
 
 export const SequenceToDrawnixSkeletonConvertor = new GraphConverter({
   converter: (chart: Sequence, config: DrawnixConfig) => {
@@ -25,7 +33,6 @@ export const SequenceToDrawnixSkeletonConvertor = new GraphConverter({
           case "line":
             plaitElement = transformToDrawnixLineElement(element, config);
             break;
-
           case "rectangle":
           case "ellipse":
             plaitElement = transformToDrawnixContainerElement(element, config);
@@ -37,6 +44,8 @@ export const SequenceToDrawnixSkeletonConvertor = new GraphConverter({
             throw `unknown type ${element.type}`;
             break;
         }
+        // 基于 origin.id 查询和 group 中 actorKeys 的对应关系
+        plaitElement.origin = element;
         if (element.type === "rectangle" && element?.subtype === "activation") {
           activations.push(plaitElement);
         } else {
@@ -82,9 +91,58 @@ export const SequenceToDrawnixSkeletonConvertor = new GraphConverter({
 
     if (chart.groups) {
       chart.groups.forEach((group) => {
-        // TODO: 没有 x,y,width,height
+        const { actorKeys, name } = group;
+        if (!actorKeys.length) {
+          return;
+        }
+        const actors = elements.filter((ele) => {
+          const element = ele as PlaitGeometry;
+          if (element.origin && element.origin.id) {
+            const hyphenIndex = element.origin.id.indexOf("-");
+            const id = element.origin.id.substring(0, hyphenIndex);
+            return actorKeys.includes(id);
+          }
+          return false;
+        });
+        const rectangle = RectangleClient.getBoundingRectangle(
+          actors.map((actor) =>
+            RectangleClient.getRectangleByPoints(actor.points!)
+          )
+        );
+        const PADDING = 60;
+        const groupRectangle = RectangleClient.inflate(rectangle, PADDING);
+        const containerElement = createGeometryElement(
+          BasicShapes.rectangle,
+          [...RectangleClient.getPoints(groupRectangle)],
+          "",
+          { fill: group.fill, strokeWidth: 1 }
+        );
+        const text = buildText(name, undefined);
+        const textSize = getTextShapeProperty({} as any, text);
+        const points = RectangleClient.getPoints(
+          RectangleClient.getRectangleByCenterPoint(
+            [
+              groupRectangle.x + groupRectangle.width / 2,
+              groupRectangle.y + 4 + textSize.height / 2,
+            ],
+            textSize.width,
+            textSize.height
+          )
+        );
+        const textElement = createGeometryElement(
+          BasicShapes.text,
+          points,
+          text
+        );
+        elements.unshift(textElement);
+        elements.unshift(containerElement);
       });
     }
+    elements.forEach((ele) => {
+      if (ele.origin) {
+        delete ele.origin;
+      }
+    });
     return { elements };
   },
 });
