f7de985f26d561133a650e7020e68ea53695b69c
pubuzhixing8 <pubuzhixing@gmail.com>
2025-03-16 15:01:55 +0800

    refactor: changelog eslintrc and so on

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -21,7 +21,6 @@ export default defineConfig({
         "./testcases/**/*",
         "../src/parser/**/*",
         "../src/graphToDrawnix.ts",
-        "./initExcalidraw.ts",
       ],
     },
   },
